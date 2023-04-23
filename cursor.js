3d54bd2f7bffecb24936e76cba088428d64c55bc
Rawat-Kamaldeep <127033027+Rawat-Kamaldeep@users.noreply.github.com>
2023-04-23 09:42:05 +0530

    Add files via upload

---
+++ cursor.js
@@ -0,0 +1,32 @@
+const blob = document.getElementById("blob")
+
+document.body.onpointermove = event =>{
+    const {clientX, clientY} = event;
+
+    blob.animate({
+        left: `${clientX}px`,
+        top: `${clientY}px`,
+    },{duration: 3000, fill: "forwards"});
+}
+
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+document.querySelector("h1").onmouseover = event =>{
+    let iterations = 0
+    
+    const interval = setInterval(()=>{
+        event.target.innerText = event.target.innerText.split("")
+        .map((letter, index) => {
+            if(index<iterations){
+                return event.target.dataset.value[index];
+            }
+            
+            return letters[Math.floor(Math.random()*26)]
+        })
+        .join("");
+
+        if (iterations >=9) clearInterval(interval);
+
+        iterations += 1 / 5;
+    }, 30);
+}
